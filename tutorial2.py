06172d5ab1c0c0b0c54c234a3a0a24d2bc36116f
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-12-22 20:12:43 +0000

    tutorial cont.

---
+++ tutorial2.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+class MyGUI:
+
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+
+        self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
+        self.label.pack(padx=10, pady=10)
+
+        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
+        self.textbox.pack(padx=10, pady=10)
+
+        self.root.mainloop()
