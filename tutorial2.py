743eb40550046721d893ec8e64851438d2ca883f
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-12-22 20:23:41 +0000

    yh

--- tutorial2.py
+++ tutorial2.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 
 class MyGUI:
@@ -12,4 +13,22 @@ class MyGUI:
         self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
         self.textbox.pack(padx=10, pady=10)
 
+        self.check_state = tk.IntVar()
+
+        self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 16), variable=self.check_state)
+        self.check.pack(padx=10, pady=10)
+
+        self.button = tk.Button(self.root, text="Show Message", font=('Arial', 18), command=self.show_message)
+        self.button.pack(padx=10, pady=10)
+
         self.root.mainloop()
+
+    def show_message(self):
+        # print(self.check_state.get())
+        if self.check_state.get() == 0:
+            print(self.textbox.get('1.0', tk.END))
+        else:
+            messagebox.showinfo(title="Message", message=self.textbox.get('1.0', tk.END))
+
+
+MyGUI()
