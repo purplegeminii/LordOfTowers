b01afe91034f4a17224786e5bc6419fa2ff25ffd
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2023-12-24 02:22:37 +0000

    simulation of health bar in tkinter

--- Player.py
+++ Player.py
@@ -18,13 +18,17 @@ class Player:
     name: str
     level: int = 1
     job_class: str = field(init=False, default_factory=assign_job)
-    health_bar: int = field(init=False)
-    mana_bar: int = field(init=False)
+    health_bar: float = field(init=False)
+    mana_bar: float = field(init=False)
+    base_hp: float = field(init=False)
+    base_mp: float = field(init=False)
     combat_power: float = field(init=False)
 
     def __post_init__(self):
         self.health_bar = [x['init_health'] for x in loaded_data if x['job_name']==self.job_class][0]
+        self.base_hp = self.health_bar
         self.mana_bar = [x['init_mana'] for x in loaded_data if x['job_name']==self.job_class][0]
+        self.base_mp = self.mana_bar
         health_multiplier = 2/3
         mana_multiplier = 3
         self.combat_power = round(((health_multiplier)*(self.health_bar))+((mana_multiplier)*(self.mana_bar)), 2)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from Player import Player
 import tkinter as tk
-from tkinter import ttk
 import timeit
 
 
 root = tk.Tk()
 root.geometry("1000x600")  # Width x Height
 root.title("LOT")
-frame = ttk.Frame(root, padding=10)
+frame = tk.Frame(root, padx=10, pady=10)
 frame.grid()
 
 
@@ -19,16 +18,34 @@ player1 = Player(name="Delali Nsiah-Asare")
 print(player1)
 print("\n")
 
+# Create a canvas to draw the health bar
+hp_canvas = tk.Canvas(frame, width=200, height=30, bg="red")
+hp_canvas.grid(column=0, row=1)
+
+# Draw the health bar
+health_value = player1.health_bar/player1.base_hp * 100
+health_bar = hp_canvas.create_rectangle(0, 0, health_value * 2, 30, fill="green")
+
+def update_health_bar():
+    global health_value, hp_canvas, health_bar
+    health_value = player1.health_bar/player1.base_hp * 100
+    hp_canvas.delete(health_bar)
+    health_bar = hp_canvas.create_rectangle(0, 0, (health_value) * 2, 30, fill="green")
+
 def greet() -> None:
     print("Button Clicked!")
 
 # Create a button widget
 btn1 = tk.Button(frame, text="Button", command=greet)
-btn1.grid(column=0, row=1)
+btn1.grid(column=0, row=2)
 
 # Quit btn
 quitbtn = tk.Button(frame, text="Quit", command=root.destroy)
-quitbtn.grid(column=1, row=1)
+quitbtn.grid(column=1, row=2)
+
+# Update health button
+update_hp_btn = tk.Button(frame, text="Update HP", command=update_health_bar)
+update_hp_btn.grid(column=0, row=3)
 
 def main() -> None:
     print("""game start""")
