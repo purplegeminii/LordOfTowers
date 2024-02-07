551291b2f2b88d18ee47301bce7ffbf55a66c57e
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-02-07 19:21:41 +0000

    yh

--- Player.py
+++ Player.py
@@ -5,15 +5,16 @@ from dataclasses import dataclass, field
 from typing import List, Optional, Dict, Tuple
 import tkinter as tk
 
-
 data = open(os.path.join("Asserts/Data", "job_class.json"))
 loaded_data: List[Dict] = json.load(data)
 jobs: List[str] = [job['job_name'] for job in loaded_data]
+
 
 def assign_file_path(player_name: str) -> str:
     player_fname = player_name.split(" ")[0]
     path = f"{player_fname}.json"
     return os.path.join("Asserts/Data", path)
+
 
 def assign_job() -> str:
     global jobs
@@ -26,6 +27,7 @@ def assign_job() -> str:
     chosen_job = random.choices(jobs, weights=normalized_weights, k=1)
     return chosen_job[0]
 
+
 def assign_random_multiplier() -> float:
     multipliers = [1.0, 1.1, 1.2, 1.3, 2.0]
     weights = [20, 31, 23, 25, 1]  # Adjust these weights based on the desired probability
@@ -36,6 +38,7 @@ def assign_random_multiplier() -> float:
 
     chosen_multiplier = random.choices(multipliers, weights=normalized_weights, k=1)
     return chosen_multiplier[0]
+
 
 def assign_mana_regen_rate(job_name: str, player_lvl: int) -> Tuple[int, int]:
     # Implement this function based on job_name
@@ -48,16 +51,21 @@ def assign_mana_regen_rate(job_name: str, player_lvl: int) -> Tuple[int, int]:
     timer_: int = 5000
     mana_: int = 30
     if job_name == 'Mage':
-        mana_ = 60; timer_ = 12
+        mana_ = 60
+        timer_ = 12
     elif job_name == 'Assassin':
-        mana_ = 70; timer_ = 10
+        mana_ = 70
+        timer_ = 10
     elif job_name == 'Warrior':
-        mana_ = 50; timer_ = 15
-        return round(50*multiplier), 15000
+        mana_ = 50
+        timer_ = 15
+        return round(50 * multiplier), 15000
     elif job_name == "no class":
-        mana_ = 30; timer_ = 5
-    return round(mana_*multiplier), timer_*1000
-    
+        mana_ = 30
+        timer_ = 5
+    return round(mana_ * multiplier), timer_ * 1000
+
+
 def assign_hp_mp_growth_rate(job_name: str) -> Tuple[int, int]:
     if job_name == 'Mage':
         return 6, 10
@@ -67,8 +75,10 @@ def assign_hp_mp_growth_rate(job_name: str) -> Tuple[int, int]:
         return 5, 9
     elif job_name == "no class":
         return 4, 12
-    
-def calculate_combat_power(health_bar: float, mana_bar: float, health_weight: float = 0.6, mana_weight: float = 0.4) -> float:
+
+
+def calculate_combat_power(health_bar: float, mana_bar: float, health_weight: float = 0.6,
+                           mana_weight: float = 0.4) -> float:
     """
     Calculates the combat power based on health and mana bars.
 
@@ -86,7 +96,6 @@ def calculate_combat_power(health_bar: float, mana_bar: float, health_weight: fl
 
 @dataclass
 class Player:
-    
     name: str
     level: int = 1
     job_class: str = field(init=False, default_factory=assign_job)
@@ -120,11 +129,13 @@ class Player:
         health_multiplier = 1 + (self.level * self.health_growth_rate / 100)
         mana_multiplier = 1 + (self.level * self.mana_growth_rate / 100)
 
-        self.base_hp = round([x['init_health'] for x in loaded_data if x['job_name'] == self.job_class][0] * assign_random_multiplier())
+        self.base_hp = round(
+            [x['init_health'] for x in loaded_data if x['job_name'] == self.job_class][0] * assign_random_multiplier())
         self.health_bar = round(self.base_hp * health_multiplier)
         self.base_hp = self.health_bar
 
-        self.base_mp = round([x['init_mana'] for x in loaded_data if x['job_name'] == self.job_class][0] * assign_random_multiplier())
+        self.base_mp = round(
+            [x['init_mana'] for x in loaded_data if x['job_name'] == self.job_class][0] * assign_random_multiplier())
         self.mana_bar = round(self.base_mp * mana_multiplier)
         self.base_mp = self.mana_bar
 
@@ -229,11 +240,11 @@ class Player:
         player_status.create_text(50, 100, text="Job:", fill="blue", font=('Arial', 16))
         player_status.create_text(140, 100, text=self.job_class, fill="blue", font=('Arial', 16))
 
-        hp = str(self.health_bar)+"/"+str(self.base_hp)
+        hp = str(self.health_bar) + "/" + str(self.base_hp)
         player_status.create_text(50, 120, text="HP:", fill="blue", font=('Arial', 16))
         player_status.create_text(130, 120, text=hp, fill="blue", font=('Arial', 16))
 
-        mp = str(self.mana_bar)+"/"+str(self.base_mp)
+        mp = str(self.mana_bar) + "/" + str(self.base_mp)
         player_status.create_text(50, 140, text="MP:", fill="blue", font=('Arial', 16))
         player_status.create_text(130, 140, text=mp, fill="blue", font=('Arial', 16))
 
@@ -241,35 +252,34 @@ class Player:
         player_status.create_text(100, 200, text=str(self.combat_power), fill="blue", font=('Arial', 16))
 
         return player_status
-    
+
     def create_hp_mp_bars(self, frame: tk.Frame) -> None:
         self.hp_mp_bars = tk.Frame(frame)
         self.hp_mp_bars.rowconfigure(0, weight=1)
         self.hp_mp_bars.rowconfigure(1, weight=1)
 
         self.hp_canvas = tk.Canvas(self.hp_mp_bars, width=200, height=30, bg="red")
-        self.hp_canvas.grid(row=0, column=0, sticky=tk.W+tk.E)
+        self.hp_canvas.grid(row=0, column=0, sticky=tk.W + tk.E)
 
-        self.health_value = self.health_bar/self.base_hp * 100
+        self.health_value = self.health_bar / self.base_hp * 100
         self.health_bar_gui = self.hp_canvas.create_rectangle(0, 0, self.health_value * 2, 30, fill="green")
 
         self.mp_canvas = tk.Canvas(self.hp_mp_bars, width=200, height=30, bg="white")
-        self.mp_canvas.grid(row=1, column=0, sticky=tk.W+tk.E)
+        self.mp_canvas.grid(row=1, column=0, sticky=tk.W + tk.E)
 
-        self.mana_value = self.mana_bar/self.base_mp * 100
+        self.mana_value = self.mana_bar / self.base_mp * 100
         self.mana_bar_gui = self.mp_canvas.create_rectangle(0, 0, self.mana_value * 2, 30, fill="blue")
 
         self.auto_regen_mp(self.mana_regen_rate, self.regen_interval_ms)
 
-    
     def update_health_bar(self) -> None:
-        self.health_value = self.health_bar/self.base_hp * 100
+        self.health_value = self.health_bar / self.base_hp * 100
         self.hp_canvas.delete(self.health_bar_gui)
         self.health_bar_gui = self.hp_canvas.create_rectangle(0, 0, (self.health_value) * 2, 30, fill="green")
         print(f"{self.__class__.__name__} class method: update HP")
 
     def update_mana_bar(self) -> None:
-        self.mana_value = self.mana_bar/self.base_mp * 100
+        self.mana_value = self.mana_bar / self.base_mp * 100
         self.mp_canvas.delete(self.mana_bar_gui)
         self.mana_bar_gui = self.mp_canvas.create_rectangle(0, 0, (self.mana_value) * 2, 30, fill="blue")
         print(f"{self.__class__.__name__} class method: update MP")
@@ -280,7 +290,7 @@ class Player:
             print(f"{self.__class__.__name__} class method: auto_regen_mp = {mana_regen_rate}")
             self.mana_bar = min(self.mana_bar + mana_regen_rate, self.base_mp)
             self.update_mana_bar()  # Update the mana bar GUI here
-        
+
         # Schedule the next regeneration
         self.hp_mp_bars.after(regen_interval_ms, lambda: self.auto_regen_mp(mana_regen_rate, regen_interval_ms))
 
@@ -315,7 +325,8 @@ class Player:
                                 print(f"{mana_cost} mana used to activate {name_of_skill} skill")
                                 self.update_mana_bar()
                                 enemy_player.receive_damage(modified_damage)
-                                print(f"{self.name} used {name_of_skill} on {enemy_player.name}, imparting {modified_damage} damage!")
+                                print(
+                                    f"{self.name} used {name_of_skill} on {enemy_player.name}, imparting {modified_damage} damage!")
                             else:
                                 print(f"{self.name} doesn't have enough mana to use {name_of_skill}!")
                     else:
@@ -323,4 +334,3 @@ class Player:
                         print(f"{self.name} used {name_of_skill} (passive skill)!")
         else:
             print("No skills available for this player.")
-
