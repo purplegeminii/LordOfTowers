ad2d99620da21c88ff993fbcdb2eced8fe7a691e
Delali Nsiah-Asare <delalinsiah.asare@gmail.com>
2024-02-07 19:33:16 +0000

    yh

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from tkinter import ttk
 import timeit
 import time
 
-
 root = tk.Tk()
 root.geometry("800x600")  # Width x Height
 root.title("LOT")
@@ -13,7 +12,6 @@ frame.grid(row=0, column=0, sticky="nsew")
 frame_width, frame_height = frame.winfo_width(), frame.winfo_height()
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
-
 
 # Create a label widget
 welcome_lbl = tk.Label(frame, text="Welcome to Lord Of Towers!")
@@ -32,12 +30,14 @@ player2.create_hp_mp_bars(frame)
 hp_mp_bars2 = player2.hp_mp_bars
 hp_mp_bars2.place(x=530, y=50)
 
+
 def show_player_status(player: Player) -> None:
     global frame
     print("Player Status window opened!")
     player_status_canvas = player.get_player_status(frame)
     player_status_canvas.place(x=280, y=50)
     frame.after(5000, player_status_canvas.destroy)
+
 
 def create_general_btns(player: Player) -> tk.Frame:
     global frame
@@ -47,11 +47,12 @@ def create_general_btns(player: Player) -> tk.Frame:
     buttonframe.columnconfigure(1, weight=1)
     # Create a button widget
     pl_status = tk.Button(buttonframe, text="Status", command=lambda: show_player_status(player))
-    pl_status.grid(row=0, column=0, sticky=tk.W+tk.E)
+    pl_status.grid(row=0, column=0, sticky=tk.W + tk.E)
     # level up button
     lvl_up_btn = tk.Button(buttonframe, text="Level Up", command=player.level_up)
-    lvl_up_btn.grid(row=0, column=1, sticky=tk.W+tk.E)
+    lvl_up_btn.grid(row=0, column=1, sticky=tk.W + tk.E)
     return buttonframe
+
 
 # general buttons frame for player 1
 generalbuttonframe1 = create_general_btns(player1)
@@ -68,18 +69,23 @@ def create_skills_btns(current_player: Player, enemy_player: Player) -> tk.Frame
     active_skill_btns = tk.Frame(frame)
     active_skill_btns.rowconfigure(0, weight=1)
     # warrior
-    warrior_btn = tk.Button(active_skill_btns, text="slash", command=lambda: current_player.use_skill("slash", enemy_player))
-    warrior_btn.grid(row=0, column=0, sticky=tk.W+tk.E)
-    # assasin
-    assasin_btn = tk.Button(active_skill_btns, text="stab", command=lambda: current_player.use_skill("stab", enemy_player))
-    assasin_btn.grid(row=0, column=1, sticky=tk.W+tk.E)
+    warrior_btn = tk.Button(active_skill_btns, text="slash",
+                            command=lambda: current_player.use_skill("slash", enemy_player))
+    warrior_btn.grid(row=0, column=0, sticky=tk.W + tk.E)
+    # assassin
+    assassin_btn = tk.Button(active_skill_btns, text="stab",
+                             command=lambda: current_player.use_skill("stab", enemy_player))
+    assassin_btn.grid(row=0, column=1, sticky=tk.W + tk.E)
     # mage
-    mage_btn = tk.Button(active_skill_btns, text="fireball", command=lambda: current_player.use_skill("fireball", enemy_player))
-    mage_btn.grid(row=0, column=2, sticky=tk.W+tk.E)
+    mage_btn = tk.Button(active_skill_btns, text="fireball",
+                         command=lambda: current_player.use_skill("fireball", enemy_player))
+    mage_btn.grid(row=0, column=2, sticky=tk.W + tk.E)
     # no class
-    no_class_btn = tk.Button(active_skill_btns, text="punch", command=lambda: current_player.use_skill("difficult punch", enemy_player))
-    no_class_btn.grid(row=0, column=3, sticky=tk.W+tk.E)
+    no_class_btn = tk.Button(active_skill_btns, text="punch",
+                             command=lambda: current_player.use_skill("difficult punch", enemy_player))
+    no_class_btn.grid(row=0, column=3, sticky=tk.W + tk.E)
     return active_skill_btns
+
 
 active_skills_1 = create_skills_btns(player1, player2)
 active_skills_1.place(x=50, y=250)
@@ -93,6 +99,7 @@ def main() -> None:
     print(player1)
     print(player2)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
